a2e27bc3eed1130140b417589e04d95835d4c2c9
Chanwoo-Jeong <lordroad38@naver.com>
2023-01-18 00:43:36 +0900

    Feat : Socket.IO 기능 설치 및 서버와의 test

--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -7,9 +7,17 @@ import android.widget.ScrollView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.testapp.databinding.ActivityMainBinding
-import kotlin.math.log
+import kotlinx.coroutines.Dispatchers.IO
+import okhttp3.WebSocket
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.net.URISyntaxException
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+
+import io.socket.client.IO;
+import io.socket.client.Socket;
+
 
 fun WhatTime(): String? {
     val dateTime = LocalDateTime.now()
@@ -20,10 +28,20 @@ fun WhatTime(): String? {
 class MainActivity : AppCompatActivity() {
     private lateinit var viewbinding: ActivityMainBinding
 
+    lateinit var mSocket: Socket
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewbinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(viewbinding.root)
+
+        try {
+            //IO.socket 메소드는 은 저 URL 을 토대로 클라이언트 객체를 Return 합니다.
+            mSocket = io.socket.client.IO.socket("http://10.0.2.2:3000")
+        } catch (e: URISyntaxException) {
+            Log.e("MainActivity", e.reason)
+        }
+        mSocket.connect()
 
         val items: ArrayList<ChatData> = arrayListOf()
 
@@ -35,14 +53,20 @@ class MainActivity : AppCompatActivity() {
 
         viewbinding.msgBtn.setOnClickListener {
             val time:String = WhatTime().toString()
-            items.apply {
-                add(ChatData(viewbinding.textmsg.text.toString(),time))
-            }
-            rvAdapter.notifyDataSetChanged()
-            //스크롤 포지션
-            rv.scrollToPosition(items.size-1)
-            viewbinding.textmsg.setText("")
+            val msg:String = viewbinding.textmsg.text.toString()
+            if(msg.isNotEmpty()) {
+                Log.d("msg",msg)
+                items.apply {
+                    add(ChatData(msg, time))
+                }
+                rvAdapter.notifyDataSetChanged()
+                //스크롤 포지션
+                rv.scrollToPosition(items.size - 1)
+                viewbinding.textmsg.setText("")
 
+                val chat = "I'm kotlin"
+                mSocket.emit("say", chat)
+            }
         }
     }
 }
